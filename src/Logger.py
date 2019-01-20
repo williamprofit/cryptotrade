13990d7eecbc192786a54a440afbe370738b61f5
William PROFIT <williamprofit@dyn3212-189.wlan.ic.ac.uk>
2019-01-20 13:03:08 +0000

    Logger now logs time for every message

--- src/Logger.py
+++ src/Logger.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Logger:
     def __init__(self):
         # List of tuples of write streams and log levels
@@ -10,7 +12,8 @@ class Logger:
     def log(self, msg, logLevel=0):
         for stream in self.streams:
             if (stream[0] == logLevel):
-                stream[1].write(msg + '\n')
+                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                stream[1].write(time + ' | ' + msg + '\n')
 
     def addTarget(self, path, logLevel=0):
         self.streams.append((logLevel, open(path, 'a')))
