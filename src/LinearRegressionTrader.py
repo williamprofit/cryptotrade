c6339d8ba4091fd00ee7d653caf98d432bbf1976
Anagh Malik <anaghmalik@gmail.com>
2019-02-05 14:38:10 +0000

    Added LinearRegressionTrader

--- src/LinearRegressionTrader.py
+++ src/LinearRegressionTrader.py
@@ -15,15 +15,18 @@ class LinearRegressionTrader(Trader):
 
   def __init__(self, portfolio, logger, log_level, metrics_list):
     super().__init__(portfolio, logger, log_level)
-
     self.metrics_list = metrics_list
-    start_time = datetime(2018, 12, 1)
-    end_time = datetime(2018, 12, 2)
+    start_time = datetime(2017, 6, 1)
+    end_time = datetime(2018, 6, 1)
     self.ass = Asset('ethereum', 'ETH')
-    self.timeframe = timedelta(minutes=5)
+    self.timeframe = timedelta(days=1)
+
+    for metric in metrics_list:
+      Metric.loadMetric(metric, self.ass, start_time, end_time, self.timeframe) 
+    
+
     current_metrics_array = self.currentMetricsArrayGenerator(self.ass, start_time, end_time)
     future_prices_list = self.futurePricesListGenerator(self.ass, start_time, end_time)
-
     self.linear_model = self.regression(current_metrics_array, future_prices_list)
 
 
@@ -46,8 +49,8 @@ class LinearRegressionTrader(Trader):
     price_list = []
     time_at_future = start_time + self.timeframe
 
-    while time_at_moment < end_time:
-      price_list.append(Metric.getMetric("price", ass, time_at_future))
+    while time_at_future < end_time:
+      price_list.append(Metric.getMetric("price", self.ass, time_at_future))
       time_at_future += self.timeframe
 
     return price_list
@@ -59,10 +62,11 @@ class LinearRegressionTrader(Trader):
     lm = sklearn.linear_model.LinearRegression()
     lm.fit(X,y)
 
+
     return lm
 
 
-  def prediction_at_time(list_of_metric_values):
+  def prediction_at_time(self, list_of_metric_values):
     prediction = self.linear_model.predict(list_of_metric_values)
 
     return prediction
@@ -70,13 +74,16 @@ class LinearRegressionTrader(Trader):
 
   def action(self):
     super().action()
+    asset = self.portfolio.getAsset('ETH')
 
     now = self.curr_time
     list_of_metric_values = []
     for metric in self.metrics_list:
       list_of_metric_values.append(Metric.getMetric(metric, self.ass, now))
-
-    curr_price = Metric.getMetric(price, self.ass, now)
+    list_of_metric_values = [list_of_metric_values]
+    
+    print(list_of_metric_values)
+    curr_price = Metric.getMetric("price", self.ass, now)
     pred_price = self.prediction_at_time(list_of_metric_values)
 
 
