9a195d509b7cb0665c36d3fa816012f529f95848
Anagh Malik <anaghmalik@gmail.com>
2019-02-06 22:38:50 +0000

    Changed LinearRegression as per new metric

--- src/LinearRegressionTrader.py
+++ src/LinearRegressionTrader.py
@@ -8,50 +8,54 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
 from datetime import datetime, timedelta
+from Portfolio import Portfolio
 
 
 
 class LinearRegressionTrader(Trader):
 
-  def __init__(self, portfolio, logger, log_level, metrics_list):
+  def __init__(self, portfolio, logger, log_level, metrics_list, asset):
     super().__init__(portfolio, logger, log_level)
     self.metrics_list = metrics_list
-    start_time = datetime(2017, 6, 1)
-    end_time = datetime(2018, 6, 1)
-    self.ass = Asset('ethereum', 'ETH')
-    self.timeframe = timedelta(days=1)
+    self.ass = asset
 
-    for metric in metrics_list:
-      Metric.loadMetric(metric, self.ass, start_time, end_time, self.timeframe) 
+
+  def train(self, start_training, end_training, timeframe):
+    for metric in self.metrics_list:
+      Metric.loadMetric(metric, self.ass, start_training, end_training, timeframe) 
     
-
-    current_metrics_array = self.currentMetricsArrayGenerator(self.ass, start_time, end_time)
-    future_prices_list = self.futurePricesListGenerator(self.ass, start_time, end_time)
+    for social_platform_number in range (0,4):
+      Metric.loadMetric("social_chart_data", self.ass, start_training, end_training, timeframe, args =[social_platform_number, "ethereum"]) 
+    
+    current_metrics_array = self.currentMetricsArrayGenerator(self.ass, start_training, end_training, timeframe)
+    future_prices_list = self.futurePricesListGenerator(self.ass, start_training, end_training, timeframe)
     self.linear_model = self.regression(current_metrics_array, future_prices_list)
 
 
-
-  def currentMetricsArrayGenerator(self, asset, start_time, end_time):
+  def currentMetricsArrayGenerator(self, asset, start_time, end_time, timeframe):
     metrics_array = []
     time_at_moment = start_time
 
-    while time_at_moment < end_time - self.timeframe:
+    while time_at_moment < end_time - timeframe:
       metrics_in_moment = []
       for metric in self.metrics_list:
-        metrics_in_moment.append(Metric.getMetric(metric, self.ass, time_at_moment))
+        metrics_in_moment.append(Metric.getMetric(metric, asset, time_at_moment))
+      for social_platform_number in range (0,4):
+         metrics_in_moment.append(Metric.getMetric(metric, asset, time_at_moment, args =[social_platform_number, "ethereum"])) 
+      
       metrics_array.append(metrics_in_moment)
-      time_at_moment += self.timeframe
+      time_at_moment += timeframe
 
     return metrics_array
 
 
-  def futurePricesListGenerator(self, asset, start_time, end_time):
+  def futurePricesListGenerator(self, asset, start_time, end_time, timeframe):
     price_list = []
-    time_at_future = start_time + self.timeframe
+    time_at_future = start_time + timeframe
 
     while time_at_future < end_time:
-      price_list.append(Metric.getMetric("price", self.ass, time_at_future))
-      time_at_future += self.timeframe
+      price_list.append(Metric.getMetric("price", asset, time_at_future))
+      time_at_future += timeframe
 
     return price_list
 
@@ -61,6 +65,8 @@ class LinearRegressionTrader(Trader):
     y = future_data
     lm = sklearn.linear_model.LinearRegression()
     lm.fit(X,y)
+    print(y)
+    print(lm.predict(X))
 
 
     return lm
@@ -80,9 +86,13 @@ class LinearRegressionTrader(Trader):
     list_of_metric_values = []
     for metric in self.metrics_list:
       list_of_metric_values.append(Metric.getMetric(metric, self.ass, now))
-    list_of_metric_values = [list_of_metric_values]
     
+    for social_platform_number in range (0,4):
+      list_of_metric_values.append(Metric.getMetric("social_chart_data", self.ass, now, args =[social_platform_number, "ethereum"])) 
+
+    list_of_metric_values = [list_of_metric_values]
     print(list_of_metric_values)
+    
     curr_price = Metric.getMetric("price", self.ass, now)
     pred_price = self.prediction_at_time(list_of_metric_values)
 
@@ -96,7 +106,6 @@ class LinearRegressionTrader(Trader):
       self.market.sell(asset, 1)
       print(pred_price)
       print(curr_price)
-
 
 
   def finalAction(self):
